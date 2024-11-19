b59fb1a74c7b9aa56290a6fcd8bde2cc2fd44ceb
SayyedAquib <sayyed.aquib89@gmail.com>
2024-11-19 19:47:08 +0530

    Setup React + Shadcn UI, App Routes and Layout, Header Component, and Dark / Light Mode

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
+      <div className="container mx-auto px-4 text-center text-gray-200">
+        <p>Made with 🧠 by Aquib</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
